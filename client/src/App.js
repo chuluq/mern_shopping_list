fa9a3da63ffc2b2e9ed4f7a887258317caacfac0
Chuluq <choirulchuluq10@gmail.com>
2019-11-16 13:26:11 +0700

    add shopping list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
+      <ShoppingList />
     </div>
   );
 }
