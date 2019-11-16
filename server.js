7534c258a36bf4fb1414d128109423f1269c6ba0
Chuluq <choirulchuluq10@gmail.com>
2019-11-16 20:24:29 +0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 require("dotenv/config");
 
 // Middlewares
@@ -18,6 +19,16 @@ mongoose.connect(
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log("MongoDB connected...")
 );
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
